bf3128e461c271b5e387a9d0a1fd396f2e4bef69
Connor <connor5741213@gmail.com>
2022-11-30 19:45:26 -0500

    installing dependencies and getting server running

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+const express = require("express");
+const app = express();
+
+// CONFIGURATION / MIDDLEWARE
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// ROOT
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to Rated",
+  });
+});
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
