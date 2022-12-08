fab2e81e0d2c0298d29fda33fb2ead8a5b895320
Connor Edwards <105730781+Connorwe@users.noreply.github.com>
2022-12-08 18:41:43 -0500

    added comment model (#8)

---
+++ migrations/20221208232916-create-comment.js
@@ -0,0 +1,29 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("comments", {
+      comment_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      movie_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      tvshow_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      videogame_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("comments");
+  },
+};
