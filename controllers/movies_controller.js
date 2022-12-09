6f7f4855aa5b41f80e0a861fc16a15bde0161a8a
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-12-08 19:21:43 -0500

    added movie details route

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -2,7 +2,7 @@ const movies = require("express").Router();
 const db = require("../models/");
 const { Movie } = db;
 
-// FIND ALL BANDS
+// FIND ALL 
 movies.get("/", async (req, res) => {
   try {
     const foundMovie = await Movie.findAll({});
@@ -11,5 +11,25 @@ movies.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+
+movies.get('/:movie_id', async (req, res) => {
+  let movie_id = Number(req.params.movie_id)
+  if (isNaN(movie_id)) {
+      res.status(404).json({ message: `Invalid id "${movie_id}"` })
+  } else {
+      const movie = await Movie.findOne({
+          where: { movie_id: movie_id },
+          // include: {
+          //     association: 'comments'
+          // }
+      })
+      if (!movie) {
+          res.status(404).json({ message: `Could not find place with id "${movie_id}"` })
+      } else {
+          res.json(movie)
+      }
+  }
+})
 
 module.exports = movies;
