644a968b7f2975e9dd6f5cdf83b55d05a9862644
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-12-08 21:39:42 -0500

    working movie routes

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -2,7 +2,12 @@ const movies = require("express").Router();
 const db = require("../models/");
 const { Movie } = db;
 
-// FIND ALL BANDS
+movies.post('/', async (req, res) => {
+  const movie = await Movie.create(req.body)
+  res.json(movie)
+})
+
+
 movies.get("/", async (req, res) => {
   try {
     const foundMovie = await Movie.findAll({});
@@ -11,5 +16,62 @@ movies.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+movies.get('/:movie_id', async (req, res) => {
+  let movie_id = Number(req.params.movie_id)
+  if (isNaN(movie_id)) {
+      res.status(404).json({ message: `Invalid id "${movie_id}"` })
+  } else {
+      const movie = await Movie.findOne({
+          where: { movie_id: movie_id },
+          // include: {
+          //     association: 'comments',
+          //     include: 'author'
+          // }
+      })
+      if (!movie) {
+          res.status(404).json({ message: `Could not find place with id "${movie_id}"` })
+      } else {
+          res.json(movie)
+      }
+  }
+})
+
+movies.put('/:movie_id', async (req, res) => {
+  let movie_id = Number(req.params.movie_id)
+  if (isNaN(movie_id)) {
+      res.status(404).json({ message: `Invalid id "${movie_id}"` })
+  } else {
+      const movie = await Movie.findOne({
+          where: { movie_id: movie_id},
+      })
+      if (!movie) {
+          res.status(404).json({ message: `Could not find place with id "${movie_id}"` })
+      } else {
+          Object.assign(movie, req.body)
+          await movie.save()
+          res.json(movie)
+      }
+  }
+})
+
+movies.delete('/:movie_id', async (req, res) => {
+  let movie_id = Number(req.params.movie_id)
+  if (isNaN(movie_id)) {
+      res.status(404).json({ message: `Invalid id "${movie_id}"` })
+  } else {
+      const movie = await Movie.findOne({
+          where: {
+            movie_id: movie_id
+          }
+      })
+      if (!movie) {
+          res.status(404).json({ message: `Could not find place with id "${movie_id}"` })
+      } else {
+          await movie.destroy()
+          res.json(movie)
+      }
+  }
+})
 
 module.exports = movies;
--- migrations/20221208232916-create-comment.js
+++ migrations/20221208232916-create-comment.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       content: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         allowNull: false,
       },
       stars: {
